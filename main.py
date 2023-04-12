fdb641e28c115b2de00678db5138a7fd6fa8b062
wilku <29576048+wilku103@users.noreply.github.com>
2023-04-12 20:44:42 +0200

    idk

--- main.py
+++ main.py
@@ -1,96 +1,187 @@
-from random import choice
+from os.path import exists
 
 import kivy
-from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
+from kivy.properties import ObjectProperty, BooleanProperty, ListProperty
 from kivy.storage.jsonstore import JsonStore
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.toolbar import MDTopAppBar
 
 kivy.require("2.1.0")
 
 
-class Position(MDBoxLayout):
+class Position(MDLabel):
+	edit = BooleanProperty(False)
 	picked = BooleanProperty(False)
-	text = StringProperty("")
+
+	textinput = ObjectProperty(None, allownone=True)
+
+	def on_touch_down(self, touch):
+		if self.collide_point(*touch.pos) and not self.edit:
+			self.edit = True
+		return super(Position, self).on_touch_down(touch)
+
+	def on_edit(self, instance, value):
+		if not value:
+			if self.textinput:
+				self.remove_widget(self.textinput)
+			return
+		self.textinput = MDTextField(
+				text=self.text, size_hint=(1, 1),
+				font_size=self.font_size, font_name=self.font_name,
+				pos=self.pos, size=self.size, multiline=False)
+		self.add_widget(self.textinput)
+		self.text = ""
+		self.textinput.bind(focus=self.on_text_focus)
+
+	def on_text_focus(self, instance, focus):
+		if focus is False:
+			if instance.text == "":
+				self.parent.parent.parent.parent.get_list().remove_position(self)
+				return
+
+			self.text = instance.text
+			self.edit = False
+
+
+class NewPosition(Position):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		# self.text = "new position"
+		self.edit = True
+		self.textinput.hint_text = "new position"
+
+	def on_text_focus(self, instance, focus):
+		if focus is False and instance.text != "":
+			self.parent.parent.parent.parent.get_list().add_position(Position(text=instance.text))
+			self.text = ""
+			self.edit = False
+
+
+class SavedList:
+	def __init__(self, name: str, path: str):
+		self.name = name
+		self.path = path
+		self.positions = []
+
+	def add_position(self, position: Position):
+		self.positions.append(position)
+
+	def remove_position(self, position: Position):
+		self.positions.remove(position)
+		position.parent.remove_widget(position)
+
+	def clear_positions(self):
+		self.positions.clear()
+
+	def get_positions(self):
+		return self.positions
+
+	def get_name(self):
+		return self.name
+
+	def get_path(self):
+		return self.path
+
+	def set_name(self, name: str):
+		self.name = name
+
+	def load(self):
+		_list = JsonStore(self.path)
+		saved_positions = _list.get("list")["positions"]
+		for position in saved_positions:
+			self.positions.append(
+					Position(text=position["text"], picked=position["picked"]))
+
+	def save(self):
+		positions_to_save = [{"picked": child.picked, "text": child.text} for child in
+		                     self.positions if child.text != ""]
+		_list = JsonStore(self.path)
+		_list.put("list", positions=positions_to_save)
+		_list.store_sync()
+
+
+class ListScreen(MDScreen):
+	positions: MDGridLayout = ObjectProperty(None)
+	toolbar: MDTopAppBar = ObjectProperty(None)
+	_list: SavedList = ObjectProperty(None, rebind=True)
+
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.bind(_list=self.update)
+
+	def set_list(self, _list: SavedList):
+		self._list = _list
+
+	def get_list(self):
+		return self._list
+
+	def update(self, instance, value):
+		print("updating")
+		self.positions.clear_widgets()
+		for position in self._list.get_positions():
+			self.positions.add_widget(position)
+
+	def on_enter(self, *args):
+		self._list.add_position(NewPosition())
+		self.update(None, None)
+
+	def on_leave(self, *args):
+		self._list.save()
+		self.positions.clear_widgets()
+
+
+class Toolbar(MDTopAppBar):
+	left_action_items = ListProperty([["arrow-left", lambda x: MDApp.get_running_app().go_back()]])
 
 
 class Randomizer(MDScreen):
-	positions_list: MDGridLayout = ObjectProperty(None)
-	list_name: MDTextField = ObjectProperty(None)
-	list_path: MDTextField = ObjectProperty(None)
-	input: MDTextField = ObjectProperty(None)
+	lists = ListProperty([])
+	corrupted_lists = ListProperty([])
 	saved_lists = JsonStore("saved_lists.json")
+	lists_grid: MDGridLayout = ObjectProperty(None)
 
-	def load_list(self, name: str):
-		if name == "":
-			return
-		if name not in self.saved_lists:
-			popup = MDDialog(title="List doesn't exist")
-			popup.open()
-			return
-		path = self.saved_lists.get(name)["path"]
-		_list = JsonStore(path)
-		positions = _list.get("list")["positions"]
-		for position in positions:
-			self.positions_list.add_widget(
-					Position(text=position["text"], picked=position["picked"], size_hint_y=None, height=50))
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.identify_lists()
+		self.load_saved_lists()
 
-	def save_list(self, name: str):
-		if name == "":
-			return
-		if name in self.saved_lists:
-			popup = MDDialog(title="List already exists", text="List already exists, please choose another name",
-			                 size_hint=(None, None),
-			                 size=(400, 400))
-			popup.open()
-			return
-		positions = [{"picked": child.picked, "text": child.text} for child in reversed(self.positions_list.children)]
-		self.saved_lists.put(name, path=f"lists/{name}.txt")
-		_list = JsonStore(f"lists/{name}.txt")
-		_list.put("list", positions=positions)
-		_list.store_sync()
+	def on_enter(self, *args):
+		if len(self.corrupted_lists) > 0:
+			text = ""
+			for name in self.corrupted_lists:
+				text += f"{name}\n"
+			popup = MDDialog(title="the following lists are corrupted:", text=text)
 
-	def add_position(self, text: str):
-		if text == "":
-			return
-		self.positions_list.add_widget(Position(text=text, size_hint_y=None, height=50))
-		self.input.text = ""  # clear the input
+	# popup.open()
 
-	def clear_list(self):
-		if len(self.positions_list.children) == 0:
-			return
-		self.positions_list.clear_widgets()
+	def identify_lists(self):
+		for name in self.saved_lists:
+			if not exists(self.saved_lists.get(name)["path"]):
+				self.corrupted_lists.append(name)
+			else:
+				self.lists.append(name)
 
-	def print_random_child(self):
-		if len(self.positions_list.children) == 0:
-			return
-		can_be_picked = [child for child in self.positions_list.children if not child.picked]
+	def open_list_screen(self, instance):
+		_list = SavedList(name=instance.text, path=self.saved_lists.get(instance.text)["path"])
+		_list.load()
+		self.manager.get_screen("list").set_list(_list)
+		self.manager.get_screen("list").toolbar.title = instance.text
+		self.manager.transition.direction = "left"
+		self.manager.current = "list"
 
-		# if no positions left, show a popup
-		if len(can_be_picked) == 0:
-			popup = MDDialog(title="No positions left", text="All positions have been picked",
-			                 size_hint=(None, None),
-			                 size=(400, 400))
-			popup.open()
-			return
-
-		picked_child = choice(can_be_picked)
-		picked_child.picked = True
-
-		# create a popup with the picked child text
-		popup = MDDialog(title="Picked position", text=picked_child.text, size_hint=(None, None),
-		                 size=(400, 400))
-		popup.open()
-
-	def import_from_txt(self, path: str):
-		with open(path, "r") as f:
-			for line in f:
-				self.add_position(line)
+	def load_saved_lists(self):
+		for name in self.lists:
+			# _list = SavedList(name=name, path=self.saved_lists.get(name)["path"])
+			# _list.load()
+			list_button = MDFlatButton(text=name, on_press=self.open_list_screen)
+			self.lists_grid.add_widget(list_button)
 
 
 class MainScreen(MDScreen):
@@ -98,12 +189,25 @@ class MainScreen(MDScreen):
 
 
 class RandomizerApp(MDApp):
-	def build(self):
-		manager = MDScreenManager()
-		manager.add_widget(MainScreen(name="main"))
-		manager.add_widget(Randomizer(name="randomizer"))
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.theme_cls.primary_palette = "BlueGray"
+		self.theme_cls.primary_hue = "500"
+		self.theme_cls.theme_style = "Dark"
 
-		return manager
+		self.manager = MDScreenManager()
+
+	def build(self):
+		self.manager.add_widget(MainScreen(name="main"))
+		self.manager.add_widget(Randomizer(name="randomizer"))
+		self.manager.add_widget(ListScreen(name="list"))
+
+		return self.manager
+
+	def go_back(self):
+		self.manager.transition.direction = "right"
+		self.manager.current = self.manager.previous()
+		self.manager.transition.direction = "left"
 
 
 if __name__ == '__main__':
